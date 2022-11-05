3e241273701f6403da01bd56b64dc60b4d4932b6
yoav mashraki <yoavmashraki@walla.com>
2022-11-05 22:59:36 +0200

    trigger volume

--- BuildingEscape1/Source/BuildingEscape1/OpenDoor.cpp
+++ BuildingEscape1/Source/BuildingEscape1/OpenDoor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "OpenDoor.h"
+#include "Engine/TriggerVolume.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
@@ -19,6 +20,11 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
 	// Find the owning Actor
 	AActor* Owner = GetOwner();
 
@@ -35,6 +41,11 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Poll the Trigger Volume
+	// If the ActorThatOpens is in the volume
+	if (PressurePlate->IsOverlappingActor(ActorThatOpens))
+	{
+		OpenDoor();
+	}
 }
 
--- BuildingEscape1/Source/BuildingEscape1/OpenDoor.h
+++ BuildingEscape1/Source/BuildingEscape1/OpenDoor.h
@@ -20,6 +20,8 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void OpenDoor();
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
@@ -30,5 +32,6 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	class ATriggerVolume* PressurePlate;
-		
+	
+	AActor* ActorThatOpens; // Remember pawn inherits from actor
 };
