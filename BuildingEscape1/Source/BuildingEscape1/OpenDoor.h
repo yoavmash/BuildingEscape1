ee659dae8edb09828816967645dd3211bbff14c6
yoav mashraki <yoavmashraki@walla.com>
2022-11-05 22:47:13 +0200

    add trigger volume

--- BuildingEscape1/Source/BuildingEscape1/OpenDoor.h
+++ BuildingEscape1/Source/BuildingEscape1/OpenDoor.h
@@ -24,5 +24,11 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+private:
+	UPROPERTY(VisibleAnywhere)
+	float OpenAngle = 90.0f;
+
+	UPROPERTY(EditAnywhere)
+	class ATriggerVolume* PressurePlate;
 		
 };
